539d1ae04cdc4e9a2d180db2a56d6fc34d3cd10a
Michael Sperber <michael@scolavisa.eu>
2021-02-14 14:59:40 +0000

    add mockdata

---
+++ src/components/SearchResults.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Zoekresultaten</h1>
+    <table>
+        <tr v-for="title in filteredTitles">
+            <td>{{ title.Name }}</td>
+        </tr>
+    </table>
+</template>
+
+<script>
+import { computed } from 'vue';
+import titles from '/data.json';
+export default {
+    props: {
+        query: {
+            type: String,
+            default: ''
+        }
+    },
+    setup (props, context) {
+        const filteredTitles = computed(() => {
+            return titles.filter(s => s.Name.toLowerCase().includes(props.query.toLowerCase()));
+        });
+        return {
+            filteredTitles
+        };
+    }    
+}
+</script>
