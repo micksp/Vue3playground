607ca95dfe994c76feded50464cab257836cf715
Michael Sperber <michael@scolavisa.eu>
2021-02-14 14:57:04 +0000

    new component and setup computed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,46 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">
-      count is: {{ count }}
-  </button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+    <h2>Filter learn Vue Articles</h2>
+    <input 
+      type='text' 
+      placeholder='Filter Search' 
+      v-model='query'
+    />
+    <button @click="reset">Reset</button>
+    <div class='queryoutput'>
+        {{ query }}
+    </div>
+    
+    <search-results class="results"  :query="query"></search-results>
 </template>
 
 <script>
+// see https://learnvue.co/2020/12/setting-up-your-first-vue3-project-vue-3-0-release/#started
+import { ref } from 'vue';
+import SearchResults from './SearchResults.vue'
 export default {
     name: 'HelloWorld',
+    components: { SearchResults },
     props: {
         msg: {
             type: String,
             default: ''
         }
     },
-    data() {
+    setup () {
+
+        const query = ref('');
+        const reset = () => {
+            query.value = '';
+        } 
         return {
-            count: 0
+            reset,
+            query
         }
     }
 }
 </script>
+<style scoped>
+.queryoutput {
+    margin-top: 2rem;
+}
+</style>
